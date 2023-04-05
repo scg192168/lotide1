72c0c0f4049be5a0a7972968ab85b78cfcd3cdee
Shyamal <scghoshbc@gmail.com>
2023-04-05 15:29:07 -0700

    without.js commited

--- without.js
+++ without.js
@@ -1,3 +1,6 @@
+//Implement without which will return a subset of a given array, removing unwanted elements
+//This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array.
+
 const without = function(array, itemsToRemove) {
   let newArray = [];
   //iterate over the given array
