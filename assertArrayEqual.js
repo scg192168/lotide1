75b6b8595c9cfa965cf492f778ce6408207897a2
Shyamal <scghoshbc@gmail.com>
2023-04-15 06:28:29 -0700

    assertObjectEqual.js committed

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,14 +1,25 @@
 //FUNCTION IMPLEMENTATION
-const eqArrays = require("./eqArray");
-
-const assertArrayEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-//TEST CODE
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
+With these functions defined, you can now use assertObjectsEqual to compare two objects and log an appropriate message to the console.For example:
+
+css
+
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }); // should log "✅✅✅ Assertion Passed: { a: '1', b: 2 } === { b: 2, a: '1' }"
+assertObjectsEqual({ a: { b: 1, c: [2, 3] } }, { a: { b: 1, c: [2, 3] } }); // should log "✅✅✅ Assertion Passed: { a: { b: 1, c: [ 2, 3 ] } } === { a: { b: 1, c: [ 2, 3 ] } }"
+assertObjectsEqual({ a: { b: 1, c: [2, 3] } }, { a: { b: 1, c: [3, 2] } }); // should log "🛑🛑🛑 Assertion Failed: { a: { b: 1, c: [ 2, 3 ] } } !== { a: { b: 1, c: [ 3, 2 ] } }
+
---
+++ assertObjectEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }); // should log "✅✅✅ Assertion Passed: { a: '1', b: 2 } === { b: 2, a: '1' }"
+assertObjectsEqual({ a: { b: 1, c: [2, 3] } }, { a: { b: 1, c: [2, 3] } }); // should log "✅✅✅ Assertion Passed: { a: { b: 1, c: [ 2, 3 ] } } === { a: { b: 1, c: [ 2, 3 ] } }"
+assertObjectsEqual({ a: { b: 1, c: [2, 3] } }, { a: { b: 1, c: [3, 2] } }); // should log "🛑🛑🛑 Assertion Failed: { a: { b: 1, c: [ 2, 3 ] } } !== { a: { b: 1, c: [ 3, 2 ] } }
